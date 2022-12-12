afaa2b978b6d9ef0a56fd470f92e607f74813709
GastonZ <gastonzappullaryu@gmail.com>
2022-12-12 15:42:59 -0300

    working in details

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import CarDetails from "./pages/CarDetails";
 import { Route, Routes } from 'react-router-dom';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+AOS.init();
 
 function App() {
   return (
