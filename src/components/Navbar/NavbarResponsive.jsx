048b114f536849c27eac603c8d96ff2b9ca8e512
GastonZ <gastonzappullaryu@gmail.com>
2022-12-12 17:41:46 -0300

    fixing colors in responsive navbar

--- src/components/Navbar/NavbarResponsive.jsx
+++ src/components/Navbar/NavbarResponsive.jsx
@@ -8,7 +8,7 @@ import Offcanvas from 'react-bootstrap/Offcanvas';
 
 function NavbarResponsive() {
 
-    const [ navbarBackground, setNavbarBackground ]= useState('bg-dark')
+    const [ navbarBackground, setNavbarBackground ]= useState('')
     useEffect(()=>{
         window.addEventListener("scroll",()=>{
             if(window.scrollY> 250){
