ba47477f383db8daec6c2bb12b81684aa511acaf
matiasecharri <matiasecharri@hotmail.com>
2022-12-13 22:28:02 -0300

    avance shoppin

--- src/pages/tiendadetails/Tiendadetails.jsx
+++ src/pages/tiendadetails/Tiendadetails.jsx
@@ -6,13 +6,28 @@ import "../tiendadetails/Tiendadetails.css";
 export default function TiendaDetails() {
   return (
     <>
-      <header className="headerTiendaDetails010">
-        <h1>Our collections</h1>
-        <h3>
-Qualified high-end items</h3>
-      </header>
+      <header className="headerTiendaDetails010"></header>
+      
+      <article className="titular010TiendaDetails">
+      <h1>Our collections</h1>
+      <h3>Qualified high-end items.</h3>
+      </article>
+      
 
-      <main className="mainTiendaDetails010"></main>
+      <main className="mainTiendaDetails010">
+      <div class="card089"><img src="https://digitalassets-shop.tesla.com/image/upload/f_auto,q_auto/v1/content/dam/tesla/studio/LIFESTYLE/SOCKS/1741613-00-A_1_2000.jpg" alt="" srcset="" /></div>
+
+      <div class="card089"><img src="https://digitalassets-shop.tesla.com/image/upload/f_auto,q_auto/v1/content/dam/tesla/studio/LIFESTYLE/SOCKS/1741613-00-A_1_2000.jpg" alt="" srcset="" /></div>
+
+      <div class="card089"><img src="https://digitalassets-shop.tesla.com/image/upload/f_auto,q_auto/v1/content/dam/tesla/studio/LIFESTYLE/SOCKS/1741613-00-A_1_2000.jpg" alt="" srcset="" /></div>
+
+      <div class="card089"><img src="https://digitalassets-shop.tesla.com/image/upload/f_auto,q_auto/v1/content/dam/tesla/studio/LIFESTYLE/SOCKS/1741613-00-A_1_2000.jpg" alt="" srcset="" /></div>
+      
+      <div class="card089"><img src="https://digitalassets-shop.tesla.com/image/upload/f_auto,q_auto/v1/content/dam/tesla/studio/LIFESTYLE/SOCKS/1741613-00-A_1_2000.jpg" alt="" srcset="" /></div>
+
+      <div class="card089"><img src="https://digitalassets-shop.tesla.com/image/upload/f_auto,q_auto/v1/content/dam/tesla/studio/LIFESTYLE/SOCKS/1741613-00-A_1_2000.jpg" alt="" srcset="" /></div>
+     
+      </main>
     </>
   );
 }
