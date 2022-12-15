dfd942324b7971d4d3bcc17d7d7925ae86908f92
GastonZ <gastonzappullaryu@gmail.com>
2022-12-15 16:36:06 -0300

    working in profile

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,11 +1,44 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import './profile.css'
+import { useSelector } from 'react-redux'
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal'
 
-function Profile() {
-  return (
-    <motion.div>
-        <div>Profile</div>
-    </motion.div>
+function Profile(props) {
+
+
+    let { name, photo } = useSelector(store => store.usuario)
+    return (
+        <motion.div>
+            <main className='main-profile-container'>
+                <div className='profile-banner'>
+                    <img className='banner-img' src={photo} alt="banner" />
+                    <button className='edit-banner-btn'> <img className='edit-icon-img' src="https://cdn.discordapp.com/attachments/1019371264860770376/1053024594048589844/icons8-compact-camera-24.png" alt="edit" />
+                     Edit banner image</button>
+                </div>
+                <div className='profile-info'>
+                    <div className='profile-section profile-img-container'>
+                        <div className='edit-img-user'>
+                            <img className='user-profile-img' src={photo} alt="profile-img" />
+                            <img className='edit-img-user-btn' src="https://cdn.discordapp.com/attachments/1019371264860770376/1053024594048589844/icons8-compact-camera-24.png" alt="edit" />
+                        </div>
+                    </div>
+                    <section className='profile-section profile-username'>
+                        <h2 className='user-profile-name'>{name}</h2>
+                        <div>
+                            <button className='edit-profile-btn'>
+                                <img className='edit-icon-img' src="https://cdn.discordapp.com/attachments/1019371264860770376/1053031104380149760/icons8-edit-24.png" alt="edit" />
+                                Edit profile</button>
+                        </div>
+                    </section>
+                </div>
+                <section>
+                    <h1>probando</h1>
+                </section>
+                        
+            </main>
+        </motion.div>
   )
 }
 
