63cc02e29c3ee2d2d9f3a32253e4b9ed615833c6
GastonZ <gastonzappullaryu@gmail.com>
2022-12-15 14:45:22 -0300

    profile page transition

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
 function Profile() {
   return (
-    <div>Profile</div>
+    <motion.div>
+        <div>Profile</div>
+    </motion.div>
   )
 }
 
