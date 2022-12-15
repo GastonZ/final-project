4db2f6f746d16ea85eb8791f984104fa603cb394
matiasecharri <matiasecharri@hotmail.com>
2022-12-15 17:48:44 -0300

    se colocaron las barras y filtro de busqueda

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import App from './App';
 import { store } from './redux/store'
 import { Provider } from 'react-redux';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+     
   <React.StrictMode>
     <Provider store={store} >
       <BrowserRouter>
@@ -14,6 +16,7 @@ root.render(
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
+       
 );
 
 
