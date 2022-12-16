5142e7710b368c624d8946a9f9f7b6fc508720de
GinoValenti <ginorvalenti@gmail.com>
2022-12-15 23:55:53 -0300

    a

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React from 'react'
 import { motion } from 'framer-motion'
 import './profile.css'
 import { useSelector } from 'react-redux'
@@ -8,11 +8,6 @@ import Modal from 'react-bootstrap/Modal'
 function Profile(props) {
 
 
-    const [show, setShow] = useState(false);
-
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-
     let { name, photo } = useSelector(store => store.usuario)
     return (
         <motion.div>
@@ -21,37 +16,27 @@ function Profile(props) {
                     <img className='banner-img' src={photo} alt="banner" />
                     <button className='edit-banner-btn'> <img className='edit-icon-img' src="https://cdn.discordapp.com/attachments/1019371264860770376/1053024594048589844/icons8-compact-camera-24.png" alt="edit" />
                      Edit banner image</button>
-                     <button className='edit-banner-btn-small'> <img className='edit-icon-img' src="https://cdn.discordapp.com/attachments/1019371264860770376/1053024594048589844/icons8-compact-camera-24.png" alt="edit" />
-                     </button>
-                    <div className='profile-info'>
-                        <div className='profile-section profile-img-container'>
-                            <div className='edit-img-user'>
-                                <img onClick={handleShow} className='user-profile-img' src={photo} alt="profile-img" />
-                                <img className='edit-img-user-btn' src="https://cdn.discordapp.com/attachments/1019371264860770376/1053024594048589844/icons8-compact-camera-24.png" alt="edit" />
-                            </div>
-                        </div>
-                        <section className='profile-section profile-username'>
-                            <h2 className='user-profile-name'>{name}</h2>
-                            <div>
-                                <button className='edit-profile-btn'>
-                                    <img className='edit-icon-img' src="https://cdn.discordapp.com/attachments/1019371264860770376/1053031104380149760/icons8-edit-24.png" alt="edit" />
-                                    Edit profile</button>
-                            </div>
-                        </section>
-                    </div>
                 </div>
-                <Modal  size="lg" show={show} onHide={handleClose}>
-                    <Modal.Header className='modal-background-profile-img'>
-                    </Modal.Header>
-                    <Modal.Body className='modal-background-profile-img'>
-                        <img className='show-modal-profile-img' src={photo} alt="tufoto" />
-                    </Modal.Body>
-                    <Modal.Footer className='modal-background-profile-img'>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
-                    </Button>
-                    </Modal.Footer>
-                </Modal>        
+                <div className='profile-info'>
+                    <div className='profile-section profile-img-container'>
+                        <div className='edit-img-user'>
+                            <img className='user-profile-img' src={photo} alt="profile-img" />
+                            <img className='edit-img-user-btn' src="https://cdn.discordapp.com/attachments/1019371264860770376/1053024594048589844/icons8-compact-camera-24.png" alt="edit" />
+                        </div>
+                    </div>
+                    <section className='profile-section profile-username'>
+                        <h2 className='user-profile-name'>{name}</h2>
+                        <div>
+                            <button className='edit-profile-btn'>
+                                <img className='edit-icon-img' src="https://cdn.discordapp.com/attachments/1019371264860770376/1053031104380149760/icons8-edit-24.png" alt="edit" />
+                                Edit profile</button>
+                        </div>
+                    </section>
+                </div>
+                <section>
+                    <h1>probando</h1>
+                </section>
+                        
             </main>
         </motion.div>
   )
