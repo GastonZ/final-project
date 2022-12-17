34387eee412c7cd593e99d387ced03519b4dd4be
GinoValenti <ginorvalenti@gmail.com>
2022-12-17 18:08:14 -0300

    doing details products

--- src/pages/EachItemDetail/EachItemDetail.jsx
+++ src/pages/EachItemDetail/EachItemDetail.jsx
@@ -1,9 +1,11 @@
 import React,{useEffect,useState} from "react";
 
 import axios from "axios";
-
+import "./eachitemdetail.css"
 import { useParams } from 'react-router-dom';
 import { BASE_URL } from "../../api/url";
+import less from "./less.png"
+import plus from "./plus.png"
 function EachItemsItems (){
     let item = useParams()
     let [filter, setFilter] = useState([])
@@ -18,9 +20,27 @@ function EachItemsItems (){
         detailItem()
       },[item])
  
-      let {name}= filter
+      let {name, image, description, price}= filter
     return(
-        <div><h1>{name}</h1></div>
+        <div className="container-eachitem">
+            <img className="image-eachitem" src={image} alt="" />
+            <div className="item-description-containerr">
+            <h2>{name}</h2>
+            <h3>${price}</h3>
+            <div>
+
+            <p className="description-tittle">Quantity</p>
+            <div className="quantity-container">
+                <img className="quantyti-items" src={less} alt="" />
+                <p className="description-tittle">1</p>
+                <img className="quantyti-itemss" src={plus} alt="" />
+            </div>
+            </div>
+            <button className="button-eachitem-cart"> Add to cart</button>
+            <p className="description-tittle">Description</p>
+<p>{description}</p>
+            </div>
+        </div>
     )
 }
 export default EachItemsItems
