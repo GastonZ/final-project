face6160973051bd402ba71d5750fff20f083862
GinoValenti <ginorvalenti@gmail.com>
2022-12-19 12:36:07 -0300

    doing count of navbar

--- src/components/CartBtn/CartBtn.jsx
+++ src/components/CartBtn/CartBtn.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './cartbtn.css'
-
+import { useSelector } from 'react-redux'
 function CartBtn() {
+  let { itemsInCart } = useSelector(store => store.cart)
   return (
     <div className='cart-btn-container'>
-        <span className='cart-quantity-icon'>0</span>
+        <span className='cart-quantity-icon'>{itemsInCart.length}</span>
         <img className='cart-btn-logo' src="https://cdn.discordapp.com/attachments/1019371264860770376/1053851578366447636/icons8-shopping-cart-64_1.png" alt="carrito-link" />
     </div>
   )
