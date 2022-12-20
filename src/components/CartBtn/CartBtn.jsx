96597d70456b141ebd7c01e0a06c6064d7c92713
GastonZ <gastonzappullaryu@gmail.com>
2022-12-20 10:56:25 -0300

    navbar cart btn responsive

--- src/components/CartBtn/CartBtn.jsx
+++ src/components/CartBtn/CartBtn.jsx
@@ -6,9 +6,13 @@ function CartBtn() {
   return (
     
     <div className='cart-btn-container'>
+        <span className='cart-quantity-icon '></span>
+        <img className='cart-btn-logo cart-quantity-lg' src="https://cdn.discordapp.com/attachments/1019371264860770376/1053851578366447636/icons8-shopping-cart-64_1.png" alt="carrito-link" />
         <span className='cart-quantity-icon'></span>
-        <img className='cart-btn-logo' src="https://cdn.discordapp.com/attachments/1019371264860770376/1053851578366447636/icons8-shopping-cart-64_1.png" alt="carrito-link" />
+        <img className='cart-btn-logo cart-quantity-md' src="https://cdn.discordapp.com/attachments/1019371264860770376/1054758226257784923/icons8-shopping-cart-64_2.png" alt="carrito-link" />
+
     </div>
+    
   )}
 
 
