3847572e7c3d5867716b66fbd600d4f5bafd2908
matiasecharri <matiasecharri@hotmail.com>
2022-12-21 03:52:19 -0300

    update shop

--- src/pages/tiendaaccesories/TiendaDetailsAccesories.jsx
+++ src/pages/tiendaaccesories/TiendaDetailsAccesories.jsx
@@ -1,135 +1,144 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CarrouselBestSellers from "../../components/CarrouselBestSellers";
 import CarrouselTienda from "../../components/CarrouselTienda08";
 import "../tiendadetails/Tiendadetails.css";
-import itemsActions from "../../redux/actions/itemsActions";
-import { Link, NavLink } from 'react-router-dom';
-import { useDispatch,useSelector } from "react-redux";
 import { Slider, TextField, ThemeProvider } from "@mui/material";
-import { createTheme } from '@mui/material/styles';
-import { useEffect,useState } from "react";
+import { createTheme } from "@mui/material/styles";
+import itemsActions from "../../redux/actions/itemsActions";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import AddToCartBtn from "../../components/AddToCartBtn/AddToCartBtn";
+import cartActions from "../../redux/actions/cartActions";
 
 const theme = createTheme({
   palette: {
     primary: {
-      light: '#cef10a',
-      main: '#cef10a',
-      dark: '#cef10a',
-      contrastText: '#fff',
+      light: "#cef10a",
+      main: "#cef10a",
+      dark: "#cef10a",
+      contrastText: "#fff",
     },
-   
   },
 });
 
+/* Esta es la tienda de ropa de hombres */
 
-
-/* Esta es la tienda de accesorios */
-
-export default function TiendaDetailsAccesories() {
-  let [filtered,setFiltered]=useState('')
-   let { getItems } = itemsActions
-  const dispatch = useDispatch()
-  
-  const { items } = useSelector((state)=> state.items)
-  let itemsFiltered = items.filter(items=>items.gender.includes("none"))
-  useEffect(()=>{
-    dispatch(getItems({filtered:filtered}))
-  },[filtered])
-
-  function listen(value){
-    
-
-  if(value.target.type==="text"){
-    setFiltered(value.target.value)
+export default function TiendaDetails() {
+  async function getItemsInCartMap() {
+    await dispatch(getItemsInCart());
   }
-}
 
+  useEffect(() => {
+    getItemsInCartMap();
+  }, []);
+
+  let { getItemsInCart } = cartActions;
+
+  let dispatch = useDispatch();
+  let [filtered, setFiltered] = useState("");
+  let { getItems } = itemsActions;
+
+  const { items } = useSelector((state) => state.items);
+  let femaleFiltered = items.filter((x) => x.gender === "none");
+  let kidshatsFiltered = femaleFiltered.filter(
+    (x) => x.category_id === "hats"
+  );
+  let femaleJacketFiltered = femaleFiltered.filter((x) => x.category_id === "interior");
+  let femaleShirtFiltered = femaleFiltered.filter((x) => x.category_id === "wheel");
+
+  useEffect(() => {
+    dispatch(getItems({ filtered: filtered }));
+  }, [filtered]);
+
+  function listen(value) {
+    if (value.target.type === "text") {
+      setFiltered(value.target.value);
+    }
+  }
+
+  function addItemToCartBtn(e) {
+    console.log(e);
+  }
 
 
   return (
     <>
       <header className="headerTiendaDetails010"></header>
 
-      <main className="mainTiendaDetails010">
-        <div className="divSlidersCont">
-        <ThemeProvider theme={theme}>
-          <TextField
-            id="outlined-basic"
-            label="Search your product"
-            variant="outlined"
-            color="primary"
-            fullWidth="1"
-            onChange={listen}
-          />
-          </ThemeProvider>
-     
-          <div className="contenedorSlidersx">
-          <p>Min: </p>
-          <div className="divSliders">
-          <ThemeProvider theme={theme}>
-            <Slider
-              defaultValue={5}
-              aria-label="Default"
-              valueLabelDisplay="auto"
-              color="primary"
-            />
-              </ThemeProvider>
+      <div className="divSlidersCont">
+        <ul>Wheels</ul>
+        <ul>Interior</ul>
              
-          </div>
-          <p>Max: </p>
-          <div className="divSliders">
-          <ThemeProvider theme={theme}>
-            <Slider
-              defaultValue={90}
-              aria-label="Default"
-              valueLabelDisplay="auto"
-              color="primary"
-            />
-              </ThemeProvider>
-          </div>
-          </div>
-        </div>
+      </div>
+      <h1 className="sectiontitle">Wheels</h1>
+      <main className="mainTiendaDetails010">
+        {femaleShirtFiltered.map((x) => {
+          return (
+            <div
+              key={x.title}
+              class="card089"
+              data-aos="fade-up"
+              data-aos-duration="2000"
+            >
+              <Link className="eachpdetailpress" to={`/detailsItem/:${x._id}`}>
+                <img src={x.picture_url} alt={x.title} srcset="" />
+              </Link>
 
-        {/* <div className="banner"  data-aos="fade-down" data-aos-duration="2000">
-           <img src="https://media.discordapp.net/attachments/763661782555426817/1052683529626075207/Pngtreeblack_and_white_icons_of_7554658.png?width=931&height=524" alt="" srcset="" />
-         
-      
-         <h1 data-aos="fade-up" data-aos-duration="2000">#HighQualityClothes</h1>
-        </div>  */}
-        {/* //empieza card */}
-        {itemsFiltered.length== 0 ? (<h2>?</h2>):
-        itemsFiltered.map((x)=>{
-return(
-  <> 
-        <div key={x._id} class="card089" data-aos="fade-up" data-aos-duration="2000">
-  <Link className="eachpdetailpress" to={`/detailsItem/:${x._id}`}>
-          <img
-            src={x.picture_url}
-            alt=""
-            srcset=""
-          /></Link>
+              <div className="cardTiendaContainer">
+                <div className="cardTiendaText">
+                  <h6>{x.title}</h6>
+                  <h6>Price: ${x.unit_price}</h6>
+                </div>
 
-          <div className="cardTiendaContainer">
-            <div className="cardTiendaText">
-              <h6>{x.title}</h6>
-              <h6>${x.unit_price}</h6>
+                <div className="cardTiendaButton">
+                  {/*                   <button className="chango"></button> */}
+                  <AddToCartBtn title={x.title} unit_price={x.unit_price} picture_url={x.picture_url} />
+                  <Link to={`/detailsItem/:${x._id}`}>
+                    <button className="lupa"></button>
+                  </Link>
+                </div>
+              </div>
             </div>
-
-            <div className="cardTiendaButton">
-            <AddToCartBtn title={x.title} unit_price={x.unit_price} picture_url={x.picture_url} />
-            <Link className="eachpdetailpress" to={`/detailsItem/:${x._id}`}> <button  className="lupa"></button></Link>
-            </div>
-          </div>
-        </div>
-        </>
-)
+          );
         })}
-        {/* //termina card */}
-        {/* //empieza card */}
-       
-        {/* //termina card */}
       </main>
+     
+      <h1 className="sectiontitle" id="main2">
+        Interior Items
+      </h1>
+      <main className="mainTiendaDetails010">
+        {femaleJacketFiltered.map((x) => {
+          return (
+            <div
+              key={x.title}
+              class="card089"
+              data-aos="fade-up"
+              data-aos-duration="2000"
+            >
+              <Link className="eachpdetailpress" to={`/detailsItem/:${x._id}`}>
+                <img src={x.picture_url} alt={x.title} srcset="" />
+              </Link>
+
+              <div className="cardTiendaContainer">
+                <div className="cardTiendaText">
+                  <h6>{x.title}</h6>
+                  <h6>Price: ${x.unit_price}</h6>
+                </div>
+
+                <div className="cardTiendaButton">
+                  {/*                   <button className="chango"></button> */}
+                  <AddToCartBtn title={x.title} unit_price={x.unit_price} picture_url={x.picture_url} />
+                  <Link to={`/detailsItem/:${x._id}`}>
+                    <button className="lupa"></button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </main>
+  
+     
     </>
   );
 }
