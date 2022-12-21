78135c2999361d019f6380743c7ed3ce75bedd8c
GinoValenti <ginorvalenti@gmail.com>
2022-12-21 20:54:02 -0300

    fixing style

--- src/components/CarouselComments/CarouselComments.jsx
+++ src/components/CarouselComments/CarouselComments.jsx
@@ -27,11 +27,29 @@ import "swiper/css/navigation";
     <>
       <Swiper
       slidesPerView={4}
-      spaceBetween={20}
+      spaceBetween={30}
       centeredSlides={true}
       autoplay={{
         delay: 2500,
         disableOnInteraction: false,
+      }}
+      breakpoints={{
+        "@0.00": {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        "@0.75": {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        "@1.00": {
+          slidesPerView: 3,
+          spaceBetween: 40,
+        },
+        "@1.50": {
+          slidesPerView: 4,
+          spaceBetween: 50,
+        },
       }}
       pagination={{
         clickable: true,
