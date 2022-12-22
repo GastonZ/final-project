a771fcb1d742e9c2ed10b86099df3da6c3efdaf6
GinoValenti <ginorvalenti@gmail.com>
2022-12-22 14:20:27 -0300

    a

--- src/components/FormCar/FormCar.jsx
+++ src/components/FormCar/FormCar.jsx
@@ -2,28 +2,6 @@ import React, {useRef} from 'react'
 import "./formcar.css"
 import emailjs from '@emailjs/browser';
 
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
-    },
-  },
-};
-const names = [
-    'Oliver Hansen',
-    'Van Henry',
-    'April Tucker',
-    'Ralph Hubbard',
-    'Omar Alexander',
-    'Carlos Abbott',
-    'Miriam Wagner',
-    'Bradley Wilkerson',
-    'Virginia Andrews',
-    'Kelly Snyder',
-  ];
 
 function FormCar() {
 
@@ -43,8 +21,8 @@ function FormCar() {
   
     
   return (
-    <div className='car-form-container'>
       <form ref={form} onSubmit={sendEmail}>
+    <div className='car-form-container'>
         <label>Name</label>
         <input type="text" name="user_name" />
         <label>Email</label>
@@ -52,7 +30,7 @@ function FormCar() {
         <label>Message</label>
         <textarea name="message" />
         <input type="submit" value="Send" />
-      </form>
+    
       <h3 className='h3-car-form'>
           Send a message if you are interesed 
       </h3>
@@ -82,6 +60,7 @@ function FormCar() {
       </button>
 
     </div>
+    </form>
   )
 }
 export default FormCar
